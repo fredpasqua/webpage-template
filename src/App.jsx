849d65fc8e9370477eeac81f3d9d17eba27dc050
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2023-10-03 07:42:35 -0400

    add button to show and hide header and banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import Header from './Components/Header/component.jsx';
 import Banner from './Components/HeroBanner/component.jsx';
 import BasicExample from "./Components/Navigation/navbar.jsx";
+import { useState } from "react";
 import "./App.css";
-const heroBanner = true;
-const header = true;
 
 function App() {
+  const [heroBanner, setHeroBanner] = useState(false);
+  const [header, setHeader] = useState(true);
+  const handleClick = () => {
+    setHeroBanner(!heroBanner);
+    setHeader(!header);
+  };
   return (
     <>
       <BasicExample />
       <div className="app-displayer">
-        {header ? <Header /> : null}
+        {header ? <Header handleClick={handleClick} /> : null}
         {heroBanner ? <Banner /> : null}
       </div>
     </>
--- src/Components/Header/component.jsx
+++ src/Components/Header/component.jsx
@@ -1,13 +1,18 @@
 import "./styles.css";
 import { Button } from "react-bootstrap";
-export default function Header() {
+export default function Header(props) {
   return (
     <>
       <div className="HeaderBackground">
         <div className="HeaderBox">
           <div className="HeaderText">
             <h2>City Tours USA</h2>
-            <Button className="Header-Button">Get Started</Button>
+            <Button
+              className="Header-Button"
+              onClick={() => props.handleClick()}
+            >
+              Get Started
+            </Button>
           </div>
         </div>
       </div>
