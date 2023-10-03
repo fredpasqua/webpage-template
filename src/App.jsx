256826e3e1d92cefe6771251d11c8e57be915682
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2023-10-03 00:39:02 -0400

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './Components/Header/component.jsx';
 import Banner from './Components/HeroBanner/component.jsx';
+import NavScrollExample from "./Components/Navigation/navbar.jsx";
 import './App.css'
 const heroBanner = true;
 const header = true; 
@@ -9,14 +10,11 @@ function App() {
 
   return (
     <>
-  {header ? 
-     <Header/>
-      : null}
-    {heroBanner ? 
-     <Banner/>
-      : null}
+      <NavScrollExample />
+      {header ? <Header /> : null}
+      {heroBanner ? <Banner /> : null}
     </>
-  )
+  );
 }
 
 export default App
