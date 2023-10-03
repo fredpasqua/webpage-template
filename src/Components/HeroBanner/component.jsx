9b8ae3dc33b46e839e5e5fe66d5e4395b72dd3a3
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2023-10-02 23:53:12 -0400

    added some text

--- src/Components/HeroBanner/component.jsx
+++ src/Components/HeroBanner/component.jsx
@@ -4,23 +4,34 @@ import 'animate.css';
 export default function Banner() {
   return (
     <>
-    <div className="background">
-<div className="parent" >
+      <div className="background">
+        <div className="parent">
+          <div className="text-element">
+            <h2 className="animate__animated animate__fadeInDown">
+              Find Your Place
+            </h2>
 
-    <div  className="text-element">
-        <h2 className="animate__animated animate__fadeInDown">Find Your Place</h2>
-      
-        <p className="animate__animated animate__fadeInUp" >Lorem ipsum. Excepturi aspernatur, nam illo voluptate necessitatibus fuga molestias eveniet, dolor placeat omnis, consectetur ea fugiat nisi minima temporibus sapiente iure. Enim, repellat.</p>
-        <p className="animate__animated animate__fadeInUp">Lorem ipsum dolor sit, amet consectetur, consectetur ea fugiat nisi minima temporibus sapiente iure. Enim, repellat.</p>
-    </div>
-    <div className="image-element"><img className="animate__animated animate__fadeInRight" src="src/Components/HeroBanner/images/girl.png"/>
+            <p className="animate__animated animate__fadeInUp">
+              Cities are excellent places. Places to find your passion, enjoy
+              art or a fine meal. You are never more than arms distance from a
+              new friend.
+            </p>
+            <p className="animate__animated animate__fadeInUp">
+              Whatever you desire, your city has something special waiting right
+              around the corner. Don't worry about that until you find something
+              else.
+            </p>
+          </div>
+          <div className="image-element">
+            <img
+              className="animate__animated animate__fadeInRight"
+              src="src/Components/HeroBanner/images/girl.png"
+            />
+          </div>
         </div>
-        
-</div>
-</div>
-
-</>
-  )
+      </div>
+    </>
+  );
 }
 
 
