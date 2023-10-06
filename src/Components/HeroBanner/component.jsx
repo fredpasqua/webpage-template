61a19f2e9570cdf34502f40e0e0dbc293259b1eb
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2023-10-06 17:27:21 -0400

    add inView

--- src/Components/HeroBanner/component.jsx
+++ src/Components/HeroBanner/component.jsx
@@ -1,28 +1,45 @@
 import './styles.css';
 import 'animate.css';
+
 import { useInView } from "react-intersection-observer";
 import girl from "../../../src/assets/images/girl.png";
+
 export default function Banner() {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    delay: 500,
+  });
+
   return (
     <>
       <div className="background">
         <div className="parent">
-          <div className="text-element">
-            <h2 ref={myRef} className="animate__animated animate__fadeInDown">
-              Find Your Place
-            </h2>
-            <p className="animate__animated animate__fadeInUp">
-              Cities provide opportunities to explore an abundance of different
-              cuisines, festivals, and art forms.
-            </p>
-            <p className="animate__animated animate__fadeInUp">
-              Opportunities to make new friends and connect with like-minded
-              individuals await.
-            </p>
+          <div
+            ref={ref}
+            className={`text-element ${
+              inView ? "animate__animated animate__fadeInUp" : "hide"
+            }`}
+          >
+            <h2 ref={ref}>Find Your Place</h2>
+            <section>
+              <p>
+                Cities provide opportunities to explore an abundance of
+                different cuisines, festivals, and art forms.
+              </p>
+              <p>
+                Opportunities to make new friends and connect with like-minded
+                individuals await.
+              </p>
+            </section>
           </div>
           <div className="image-element">
             <img
-              className="animate__animated animate__fadeInRight brightness"
+              ref={ref}
+              className={
+                inView
+                  ? "animate__animated animate__fadeInRight brightness"
+                  : ""
+              }
               src={girl}
             />
           </div>
