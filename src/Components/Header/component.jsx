667aecd94c52840fbf6f9f34439f92485abd25f8
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2023-10-14 22:37:03 -0400

    update layout

--- src/Components/Header/component.jsx
+++ src/Components/Header/component.jsx
@@ -1,19 +1,19 @@
 import "./styles.css";
+import walking from "../../../src/assets/images/citywalking.jpg";
 import { Button } from "react-bootstrap";
 export default function Header(props) {
   return (
     <>
-      <div className="HeaderBackground">
-        <div className="HeaderBox">
-          <div className="HeaderText">
-            <h2>City Tours USA</h2>
-            <Button
-              className="Header-Button"
-              onClick={() => props.handleClick()}
-            >
-              Get Started
-            </Button>
-          </div>
+      <div className="HeaderBox">
+        <div className="HeaderText">
+          <h2>City Tours USA</h2>
+          <p>Explore your favorite city in a whole new way.</p>
+          <Button className="Header-Button" onClick={() => props.handleClick()}>
+            Book A Tour
+          </Button>
+        </div>
+        <div className="HeaderBoxImage">
+          <img src={walking} alt="couple walking"></img>
         </div>
       </div>
     </>
