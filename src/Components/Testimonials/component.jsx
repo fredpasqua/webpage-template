ab6f13a11ba09750bfb4e851d5e8e4528035c050
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2023-10-28 14:26:17 -0400

    change

--- src/Components/Testimonials/component.jsx
+++ src/Components/Testimonials/component.jsx
@@ -35,7 +35,7 @@ const CityTourReviews = () => {
           <motion.div
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
-            transition={{ delay: 0.4, time: 1.5 }}
+            transition={{ delay: 0.4, time: 1 }}
             className="review-box"
             key={review.id}
           >
