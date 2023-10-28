d5cc899066815765aae6636b2c763815e36a5278
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2023-10-28 11:54:20 -0400

    updates

--- src/Components/Header/component.jsx
+++ src/Components/Header/component.jsx
@@ -6,6 +6,9 @@ export default function Header(props) {
     <>
       <div className="HeaderBox">
         <div className="HeaderText">
+          <div className="HeaderBoxImage">
+            <img src={walking} alt="couple walking"></img>
+          </div>
           <h2>Make a connection</h2>
           <p>
             We provide the best and most creative tours that allow you to
@@ -14,9 +17,6 @@ export default function Header(props) {
           <Button className="Header-Button" onClick={() => props.handleClick()}>
             Book A Tour
           </Button>
-        </div>
-        <div className="HeaderBoxImage">
-          <img src={walking} alt="couple walking"></img>
         </div>
       </div>
     </>
